d38b51b37ae10d7186f14076c4fc169192ef2b8c
Андрей Лещенко <Leshenko.a.v.99@gmail.com>
2019-12-02 21:54:13 +0200

    Добавьте файлы проекта.

---
+++ Dom 2.2.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dom_2._2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ФИО: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите Возраст: ");
+            int age = int.Parse (Console.ReadLine());
+            Console.Write("Введите Адрес: ");
+            string address = Console.ReadLine();
+            Console.Write("Введите Телефон: ");
+            string phone = Console.ReadLine();
+            //var phone = Console.ReadLine();
+            Console.Write("Введите Рост: ");
+            //var height = Console.ReadLine();
+            string height = Console.ReadLine();
+            Console.Write("ФИО: {0} \nВозраст: {1} \nАдрес: {2} \nТелефон: {3} \nРост: {4} \n", name, age, address, phone, height);
+
+            
+            Console.ReadKey();
+
+        }
+    }
+}
